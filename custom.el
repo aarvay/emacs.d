9f73f629d5e7678824a44fdbad996748c78d7a95
Vignesh Rajagopalan <me@aarvay.com>
2017-03-13 03:53:16 +0530

    Start tracking custom.el

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (whitespace-cleanup-mode smex ido-ubiquitous ido-vertical-mode flycheck multiple-cursors web-mode use-package toml-mode smartparens rust-mode projectile powerline markdown-mode magit-popup highlight-numbers flx-ido expand-region emmet-mode drag-stuff alchemist ag))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
