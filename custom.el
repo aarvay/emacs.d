c392b297ed44c1151ba286a3d6ec2bb5294c1610
Vignesh Rajagopalan <me@aarvay.com>
2017-03-14 10:12:40 +0530

    Remember solarized dark and light for easy switching

--- custom.el
+++ custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages
    (quote
     (multiple-cursors expand-region toml-mode rust-mode projectile emmet-mode web-mode alchemist elixir-mode markdown-mode smartparens company drag-stuff highlight-numbers smex ido-vertical-mode flx-ido ido-ubiquitous use-package))))
